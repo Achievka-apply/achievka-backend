6e44d9a0a532271d14520a703f9fb1ea97d73afa
byessirk <beksultanestirkegen@gmail.com>
2025-05-10 16:17:29 +0500

    feat/migrate to psql

--- achievka_backend/settings.py
+++ achievka_backend/settings.py
@@ -2,6 +2,11 @@ from pathlib import Path
 from datetime import timedelta
 
 import os
+import environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()  # читает .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,13 +145,20 @@ WSGI_APPLICATION = 'achievka_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'achievka_db',
+        'USER': 'achievka',
+        'PASSWORD': 'pg_achievka',
+        'HOST': '0.0.0.0',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
