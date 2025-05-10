12b75494b3d3f6e152d1e894e99f6739fd0ddb22
Yessirkegen <105101596+Yessirkegen@users.noreply.github.com>
2025-05-10 21:32:21 +0500

    Update settings.py

--- achievka_backend/settings.py
+++ achievka_backend/settings.py
@@ -70,9 +70,9 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    #"http://localhost:3000",
-    #"http://127.0.0.1:3000",
-    '*'
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    
 ]
 
 # все разрешённые callback’и
