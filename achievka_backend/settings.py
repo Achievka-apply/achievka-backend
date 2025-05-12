01ee65cf43892087657de2e8c79e032d19c71260
Yessirkegen <105101596+Yessirkegen@users.noreply.github.com>
2025-05-12 19:44:18 +0500

    Update settings.py

--- achievka_backend/settings.py
+++ achievka_backend/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "achievka.com",
     "www.achievka.com",
+    "https://achievka.com",
 ]
 
 
@@ -75,7 +76,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://51.20.95.96",      # ваш фронтенд в браузере
     "http://localhost:3000",    #для локальной разработки
-    "https://achievka.com",
+    
 ]
 CORS_ALLOW_CREDENTIALS = True    # разрешаем куки/credentials
 # все разрешённые callback’и
