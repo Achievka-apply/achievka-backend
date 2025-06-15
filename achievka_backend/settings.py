123bdaa52cc090c92ea7673a9e1127f07820e6fc
byessirk <beksultanestirkegen@gmail.com>
2025-06-15 16:19:27 +0500

    fix/OuathGoogle

--- achievka_backend/settings.py
+++ achievka_backend/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=6000),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
     # включить ротацию refresh-токенов
